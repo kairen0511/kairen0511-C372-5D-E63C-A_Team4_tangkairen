56d75e29d973b9de1c3491809e32f53f8d6573f0
kairen0511 <tangkairen75@gmail.com>
2024-10-25 16:06:08 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const multer = require('multer');
+const app = express();
+//require controllers
+
+// Set up multer for file uploads
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images'); // Directory to save uploaded files
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname); 
+    }
+});
+
+const upload = multer({ storage: storage });
+
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+});
+
+// Set up view engine
+app.set('view engine', 'ejs');
+//  enable static files
+app.use(express.static('public'));
+// enable form processing
+app.use(express.urlencoded({
+    extended: false
+}));
+
+// Define routes here
+
+
+
+//Start express server and bind it to a port
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
